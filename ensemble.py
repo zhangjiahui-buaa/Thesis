e674bfdc230fa8cd958de6a3eceaa307ffd98529
zhangjiahui-buaa <17231043@buaa.edu.cn>
2021-05-07 10:34:36 +0800

    finish ensemble

--- ensemble.py
+++ ensemble.py
@@ -1,29 +1,55 @@
 import json
 import math
-
-files = [json.load(open("model1.json", 'r')),
-         json.load(open("model2.json", 'r')),
-         json.load(open("model3.json", 'r'))]
-
-ensemble = {}
+from torchmetrics import AUROC
+import torch
+pred_files = [json.load(open("pred_tnt.json", 'r')),
+              json.load(open("pred_vit.json", 'r')),
+              json.load(open("pred_cnn.json", 'r')),
+              json.load(open("pred_pit.json", 'r')),
+              json.load(open("pred_swint.json", 'r')),
+              ]
+prob_files = [json.load(open("prob_tnt.json", 'r')),
+              json.load(open("prob_vit.json", 'r')),
+              json.load(open("prob_cnn.json", 'r')),
+              json.load(open("prob_pit.json", 'r')),
+              json.load(open("prob_swint.json", 'r')), ]
+ensemble_pred = {}
+ensemble_prob = {}
 result = {}
+all_prob = []
+all_label = []
 label = json.load(open("label.json", 'r'))
-for file in files:
+
+for i, file in enumerate(pred_files):
     for _id in file.keys():
-        if _id in ensemble:
-            ensemble[_id].append(file[_id])
+        if _id in ensemble_pred:
+            ensemble_pred[_id].append(file[_id])
+            ensemble_prob[_id].append(prob_files[i][_id])
         else:
-            ensemble[_id] = [file[_id]]
+            ensemble_pred[_id] = [file[_id]]
+            ensemble_prob[_id] = [prob_files[i][_id]]
 total = 0
 correct = 0
-for _id in ensemble.keys():
-    votes = ensemble[_id]
+for _id in ensemble_pred.keys():
+    votes = ensemble_pred[_id]
     if math.fsum(votes) < 3:
         result[_id] = 0
     else:
         result[_id] = 1
     if result[_id] == label[_id]:
         correct += 1
-    total += 1
 
-print("ensemble accuracy".format(total / correct))
+    gap = 0
+    res = [0.5, 0.5]
+    for item in ensemble_prob[_id]:
+        if item[int(result[_id])] > 0.5 and math.fabs(item[0] - item[1]) > gap:
+            gap = math.fabs(item[0] - item[1])
+            res = item
+    all_prob.append(res)
+    all_label.append(label[_id])
+
+    total += 1
+auroc = AUROC(num_classes=2, pos_label=1)
+auroc_res = auroc(torch.tensor(all_prob), torch.tensor(all_label))
+print("ensemble accuracy: {}".format(correct / total))
+print("ensemble auroc: {}".format(auroc_res))
--- readme.md
+++ readme.md
@@ -48,20 +48,30 @@
 
 1. Separate(DC)
 
-|          | Resnet52 | Vanilla Vit | Vit  | Vanilla Swint | Swint | Vanilla TNT | TNT  | Vanilla PiT | PiT  |
-| -------- | -------- | ----------- | ---- | ------------- | ----- | ----------- | ---- | ----------- | ---- |
-| Accuracy | 59.8     | 57.4        | 61.2 | 56.60         | 60.8  | 55.40       | 60.4 | 58.00       |      |
-| AUROC    |          |             |      | 62.86         |       | 62.82       |      | 63.28       |      |
+|          | Resnet152 | Vanilla Vit | Vit   | Vanilla Swint | Swint | Vanilla TNT | TNT   | Vanilla PiT | PiT   |
+| -------- | --------- | ----------- | ----- | ------------- | ----- | ----------- | ----- | ----------- | ----- |
+| Accuracy | 60.40     | 57.4        | 61.60 | 56.60         | 60.00 | 55.40       | 59.40 | 58.00       | 60.60 |
+| AUROC    | 62.90     | 62.78       | 66.80 | 62.86         | 66.20 | 62.82       | 65.37 | 63.28       | 65.66 |
 
-2. Separate(LTC)
-3. Separate(STC)
-4. Together(MMBT Style)
+2. Ensemble Separate
+
+   |          | Resnet+Vit+Swint | Vit+Swint+TNT | Resnet+Vit+Swint+Tnt+Pit |
+   | -------- | ---------------- | ------------- | ------------------------ |
+   | Accuracy | 62.40            | 59.20         | 60.20                    |
+   | AUROC    | 67.11            | 65.49         | 66.25                    |
+
+    
+
+3. Separate(LTC)
+
+4. Separate(STC)
+
+5. Together(MMBT Style)
 
 |          | Resnet152 | Vit   | Swint | TNT   | PiT   |
 | -------- | --------- | ----- | ----- | ----- | ----- |
 | Accuracy | 60.60     | 62.40 | 61.40 | 63.80 | 62.60 |
 | AUROC    | 65.57     | 68.79 | 67.40 | 66.78 | 66.92 |
-
 
 ### hateful memes unzip password: EWryfbZyNviilcDF
 
